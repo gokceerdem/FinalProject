6103accf3ba8b0bb7337e15a7d88f58b82e55b63
gokceerdem <gokcerdem90@gmail.com>
2021-03-03 22:04:26 +0300

    xxx

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //loosely coupled
+
+        //IoC container --inversion of control
+        IProductService _productService; //naming convention: private objeler için bu şekilde isimlendirilir
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        /// 
+        /// controller : sisteme yapılabileck olan istekler yazılır
+        /// sistemi kullanacak olan clientlar hangi operasyonlar için ve nasıl istekte bulunabilir?
+        /// bir web uygulaması mvc ile yazılmışsa enter'a basıp web sitesine gidince ne yapılacağına controller karar verir
+        ///
+        /*
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product{ProductId= 1, ProductName = "Elma"},
+                new Product{ProductId= 2, ProductName = "Armut"}
+            };
+        }
+        
+        [HttpGet]
+        public List<Product> Get()
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+
+            return result.Data;
+
+        }
+        */
+        //dependency chain
+
+        //gelebilecek taleplere göre kodlama yapılır
+        //ya da ne çıkacağını bilmeden SOLID prensiplerine uygun geliştirme yapılır.
+        [HttpGet]
+        /*public List<Product> Get()
+        { 
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+        */
+
+        //farklı http statü kodu döndürme olanağı kodun yeni hali:
+        public IActionResult Get()
+        {//swagger dökümantasyon
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);//200 kod başarılı döndüğü yer
+                                  //postman -->     "success": true,
+                                  //                "message": "Ürünler listelendi"
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product) //client, angular, react'ten gönderilen ürün buraya koyulur
+        {
+            //post request'te gönderi verilir, bir bilgi yollanır
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //aşağıdaki yapıyı(services.AddControllers(),services.AddSingleton ) farklı bir mimariye taşıyacağız.
+            //Bu mimariler : dotnet builtin ioc container yapısı yokken kullanılıyordu
+            //Autofac, ninject, castlewindsor,structuremap, lightinject, dryinject
+            //AOP kodlama yapacağız 
+            services.AddControllers();
+            services.AddSingleton<IProductService,ProductManager>(); //arka planda referans oluşturur
+                                                                     //IOC new() yapar
+                                                                     //birisi IProductService isterse arka planda new ProductManager oluştur bunu ver
+                                                                     //1 tane ProductManager oluşturuyor 1000 tane client bile gelse aynı productmanager kullanılacak
+                                                                     //fakat data tutmuyan bir yapı olması lazım
+                                                                     //örneğin sepet instance için kullanamazsınız yoksa tüm müşterilerin sepetleri birbirine girer
+            services.AddSingleton<IProductDal, EfProductDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
